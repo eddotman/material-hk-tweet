b198b8ca4779f6416684a90d69b5b0243f29e4e4
Edward Kim <eddotman@gmail.com>
2015-01-24 21:19:07 -0500

    Tweeting works

--- tweeter.py
+++ tweeter.py
@@ -1,14 +1,18 @@
 from pymatgen import (MPRester, Composition)
+from os import environ
 import tweepy as tp
 
 def str_kisses_per_mol(chemical):
   KISS_ENERGY = 4.75E23
   AVOGADRO = 6.02E23
 
-  rester = MPRester()
-  comp = Composition(chemical)
+  try:
+    rester = MPRester()
+    comp = Composition(chemical)
 
-  entries = rester.get_entries_in_chemsys([str(elem) for elem in comp.elements])
+    entries = rester.get_entries_in_chemsys([str(elem) for elem in comp.elements])
+  except:
+    return "Please tweet a chemical formula to me! I didn't understand your last request. :("
 
   for entry in entries:
     if entry.composition == comp:
@@ -20,7 +24,7 @@ def str_kisses_per_mol(chemical):
         return "I couldn't find the energy to make " + chemical + "."
 
   try:
-    num_kisses = round(form_energy * n_atoms * AVOGADRO / KISS_ENERGY, 8)
+    num_kisses = round(form_energy * n_atoms * AVOGADRO / KISS_ENERGY, 2)
   except:
     return "I couldn't figure out the energy to make " + chemical + "."
 
@@ -31,11 +35,18 @@ def str_kisses_per_mol(chemical):
   else:
     return "I'll give you " + str(abs(num_kisses)) + " Hershey's Kisses from forming 1 mol of " + chemical + "."
 
-def tweet_reply_kisses(in_tweet=None):
-  try:
-    auth = tp.OAuthHandler(consumer_token, consumer_secret)
-    redirect_url = auth.get_authorization_url()
-  except tp.TweepError:
-    print 'Error! Failed to get request token.'
+def tweet_reply_kisses():
+  auth = tp.OAuthHandler("API", "API")
+  auth.set_access_token("API", "API")
+  api = tp.API(auth)
+
+  public_tweets = api.mentions_timeline(count=1)
+  for tweet in public_tweets:
+      reply_to_status_id = str(tweet.id)
+      reply_to_username = str(tweet.user.screen_name)
+      chemical = str(tweet.text.encode('utf-8').replace('@mat_e_tweeter', '').strip(' '))
+
+  api.update_status("@" + reply_to_username + " " + str_kisses_per_mol(chemical), reply_to_status_id)
+
 
 tweet_reply_kisses()
